ac1ab09f78d97b5282890d551cde5bede4ac18e9
mubarakdalvi <127726167+mubarakdalvi@users.noreply.github.com>
2023-09-12 20:56:12 +0530

    Whats prolem with this code can anyone help me

--- mian.py
+++ mian.py
@@ -12,15 +12,11 @@ class Customer:
     def delete_account(self,name,customer_id,phone,address,account):
         if account in self.account:
             self.account.remove(account)
-            self.name.remove(name)
-            self.address.remove(address)
-            self.customer_id.remove(customer_id)
-            self.phone.remove(phone)
-            print(f'The account is {name}{address}{account}{customer_id}{phone} is deleted successfully, if you try to place a delete requested again it will be denied')
+            print(f'The account is {account} is deleted successfully, if you try to place a delete requested again it will be denied')
         else:
             print('Account does not exist')
     def __del__(self):
         print('This account is deleted successfully','please dont try to delete customer details if found it is under procossed for deletion')
 c1 = Customer()
 c1.open_account(name='Mubarak Dalvi',phone='9146755486',customer_id='420',account='32542',address='flat,125')
-c1.delete_account('Mubarak Dalvi',420,9146755486,'flat,125',32542)
+c1.delete_account('Mubarak Dalvi',420,'9146755486','flat,125','32542' )
