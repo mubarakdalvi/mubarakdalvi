f790f4a58f5a66a82203d4b1cf23ef149492bd72
mubarakdalvi <127726167+mubarakdalvi@users.noreply.github.com>
2023-09-13 11:24:28 +0530

    changes no 4

--- otpgenerator.py
+++ otpgenerator.py
@@ -1,21 +1,23 @@
 from random import randint
 
 otp = randint(1111, 9999)
-print('your otp is ', otp)
+print('your otp is :', otp)
 attempt = 1
 while attempt <= 3:
     try:
         attempt += 1
-        user_input = int(input('Enter Your otp'))
+        user_input = int(input('Enter Your otp :'))
         if user_input == otp:
-            print('Successfully Verified')
+            print('Successfully Verified the otp')
             break
         else:
-            if user_input < 4:
+            if attempt < 4:
                 print(" 'oh' wrong otp, please try again")
             if attempt == 4:
                 print('your card is blocked')
     except:
         print('you have tried maximum number of attempts')
+        if attempt < 4:
+            print(" 'oh' wrong otp, please try again")
         if attempt == 4:
             print('your card is blocked')
