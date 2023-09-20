6ce52c88467755d7e25c2e927c5696ac975a44b0
mubarakdalvi <127726167+mubarakdalvi@users.noreply.github.com>
2023-09-20 17:07:25 +0530

    NEW Test1

--- test1.py
+++ test1.py
@@ -12,7 +12,7 @@ print(a + b)
 print(a - b)
 print(a * b)
 
-'''code to find weird or not werid by if else'''
+'''code to find weird or not weird by if else'''
 if __name__ == '__main__':
     n = int(input().strip())
     if n % 2 or 6 <= n <= 20:
@@ -43,3 +43,29 @@ def is_leap(year):
 
 year = int(input())
 print(is_leap(year))
+
+'''code for using import re method to find str in variable '''
+# no trouble
+a = 'i am in pune and i am in maharashtra and also i am in india'
+b = ['i', 'am', 'pune', 'and', 'maharashtra', 'india', 'also', 'in']
+for x in b:
+    print('Searched For : ', x, 'and found:', re.findall(x, a))
+
+# code for checking repeating alphabets
+s = '....abf....'
+v = re.findall('a+b+', s)
+print(v)
+# code for using try except and finally in to check a is = to b or not via re module and findall () method
+try:
+    a = 'i am  Pune'
+    b = 'i Pune'
+    print(re.findall(b, a))
+    if a == b:
+        print('Yeah Its a Match')
+    else:
+        print('No its not a match')
+except:
+    print('Ahh Here We Go Again')
+finally:
+    print('thank for taking part')
+
