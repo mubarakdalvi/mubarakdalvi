92a558906d099cfcfef1f752073b51054a9723f1
mubarakdalvi <127726167+mubarakdalvi@users.noreply.github.com>
2023-10-23 19:45:12 +0530

    committed and will be updated in fututre for use

---
+++ default dict simple syntax.py
@@ -0,0 +1,8 @@
+from collections import defaultdict
+dict1 = defaultdict(list)
+dict1['python'].append('is best')
+dict1['python'].append('and is awesome language')
+if __name__ == '__main__':
+    if __name__ == '__main__':
+        for i in dict1.items():
+            print(i)
