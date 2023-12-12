017c0bdbd991b27db5b53cd1ae44289d385fff4a
mubarakdalvi <127726167+mubarakdalvi@users.noreply.github.com>
2023-12-12 13:55:48 +0530

    soltion of bigger string using printing using for loop, with jsut one input

--- pro15.py
+++ pro15.py
@@ -1,4 +1,5 @@
-
+# bigger string using one input and for loop and l just and rjust and center metnod
+'''
 word = 'H'
 width = int(input('Enter Number : '))
 
@@ -18,3 +19,16 @@ for i in range(width + 1):
 for i in range(width):
     print(((word * (width - i - 1)).rjust(width) + word +
           (word * (width - i - 1)).ljust(width)).rjust(width * 6))
+'''
+
+# using lst comprehension and \n we did this job
+'''
+def wrap(s,w):
+    s = str(s)
+    w = int(w)
+    return '\n'.join(s[i:i+w] for i in range(0,len(s),w))
+
+s = input('Enter String : ')
+w = int(input('Enter Number : '))
+print(wrap(s,w))
+'''
